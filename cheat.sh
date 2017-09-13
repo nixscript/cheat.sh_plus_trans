23c721d2fcfda3ac3aa961401cabf2452b140e15
GrigRUSS <grigruss@ya.ru>
2017-09-13 20:29:21 +0300

    Update cheat.sh

--- cheat.sh
+++ cheat.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# This is free and unencumbered software released into the public domain.
-# For more information, please refer to https://unlicense.org/
 # Author https://github.com/grigruss
 
 # Check translate-shell script
