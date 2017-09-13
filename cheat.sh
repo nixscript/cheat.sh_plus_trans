f769fc9706b871d1044d5f9b8cf29f008cf45292
GrigRUSS <grigruss@ya.ru>
2017-09-13 18:01:41 +0300

    Create cheat.sh

---
+++ cheat.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# This is free and unencumbered software released into the public domain.
+# For more information, please refer to https://unlicense.org/
+# Author https://github.com/grigruss
+
+# Check translate-shell script
+if [[ ! `trans -V` ]]
+then
+        echo -e "\nWARNING!\n========"
+        echo -e "To run the script, you need a 'trans' script from this repository:"
+        echo -e "https://github.com/soimort/translate-shell/releases\n"
+        exit 1
+fi
+
+# Check curl
+if [[ ! `curl -V` ]]
+then
+        echo -e "\nWARNING!\n========"
+        echo -e "To run the script, you need install 'curl'"
+        exit 1
+fi
+
+lang=`locale | grep -i lang`
+lang="${lang:5:2}"
+
+curl cheat.sh/$1 | while read src; do
+        if [[ ${src:11:1} == "#" ]]
+        then
+                flag="no"
+                srclen=`expr length "$src"`
+                srclen=`expr $srclen - 17`
+                echo "${src:0:11} # [en] ${src:13:$srclen}"
+                trans "${src:13:$srclen}" | while read answ; do
+                        fullanswlen=`expr length "$answ"`
+                        if [[ $fullanswlen -eq 0 ]]
+                        then
+                                flag="yes"
+                        elif [[ $flag == "yes" ]]
+                        then
+                                answlen=`expr "$fullanswlen" - 15`
+                                echo "${src:0:11} # [$lang] ${answ:4:$answlen}"
+                                break
+                        fi
+                done
+        else
+                echo $src
+        fi
+done
