26ffc9b31c412ae8402033bb80590c199780be1a
GrigRUSS <grigruss@ya.ru>
2017-09-20 21:59:22 +0300

    Update cheat.sh

--- cheat.sh
+++ cheat.sh
@@ -2,7 +2,7 @@
 # Author https://github.com/grigruss
 
 # Check translate-shell script
-if [[ ! `trans -V` ]]
+if [[ ! $(trans -V) ]]
 then
         echo -e "\n\e[33mWARNING!\n========\e[0m"
         echo -e "To run the script, you need a 'trans' script from this repository:"
@@ -11,36 +11,36 @@ then
 fi
 
 # Check curl
-if [[ ! `curl -V` ]]
+if [[ ! $(curl -V) ]]
 then
         echo -e "\n\e[33mWARNING!\n========\e[0m"
         echo -e "To run the script, you need install 'curl'"
         exit 1
 fi
 
-lang=`locale | grep -i lang`
+lang=$(locale | grep -i lang)
 lang="${lang:5:2}"
 
-curl cheat.sh/$1 | while read src; do
+curl "cheat.sh/$1" | while read -r src; do
         if [[ ${src:11:1} == "#" ]]
         then
                 flag="no"
-                srclen=`expr length "$src"`
-                srclen=`expr $srclen - 17`
-                echo "${src:0:11} # [en] ${src:13:$srclen}"
-                trans "${src:13:$srclen}" | while read answ; do
-                        fullanswlen=`expr length "$answ"`
+                srclen=${#src}
+                srclen=(srclen - 17)
+                echo "${src:0:11} # [en] ${src:13:srclen}"
+                trans "${src:13:srclen}" | while read -r answ; do
+                        fullanswlen="${#answ}"
                         if [[ $fullanswlen -eq 0 ]]
                         then
                                 flag="yes"
                         elif [[ $flag == "yes" ]]
                         then
-                                answlen=`expr "$fullanswlen" - 15`
-                                echo "${src:0:11} # [$lang] ${answ:4:$answlen}"
+                                answlen=(fullanswlen - 15)
+                                echo "${src:0:11} # [$lang] ${answ:4:answlen}"
                                 break
                         fi
                 done
         else
-                echo $src
+                echo "$src"
         fi
 done
