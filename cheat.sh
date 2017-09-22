15e6a64fcfac35872e4f3cca46012f4fce52850e
grigruss <grigruss@ya.ru>
2017-09-22 17:27:06 +0300

    Fix syntax problem

--- cheat.sh
+++ cheat.sh
@@ -4,16 +4,16 @@
 # Check translate-shell script
 if [[ ! $(trans -V) ]]
 then
-        echo -e "\n\e[33mWARNING!\n========\e[0m"
+        echo -e "\\n\\e[33mWARNING!\\n========\\e[0m"
         echo -e "To run the script, you need a 'trans' script from this repository:"
-        echo -e "https://github.com/soimort/translate-shell/releases\n"
+        echo -e "https://github.com/soimort/translate-shell/releases\\n"
         exit 1
 fi
 
 # Check curl
 if [[ ! $(curl -V) ]]
 then
-        echo -e "\n\e[33mWARNING!\n========\e[0m"
+        echo -e "\\n\\e[33mWARNING!\\n========\\e[0m"
         echo -e "To run the script, you need install 'curl'"
         exit 1
 fi
@@ -24,21 +24,17 @@ lang="${lang:5:2}"
 curl "cheat.sh/$1" | while read -r src; do
         if [[ ${src:11:1} == "#" ]]
         then
-                flag="no"
-                srclen=${#src}
-                srclen=(srclen - 17)
-                echo "${src:0:11} # [en] ${src:13:srclen}"
-                trans "${src:13:srclen}" | while read -r answ; do
-                        fullanswlen="${#answ}"
-                        if [[ $fullanswlen -eq 0 ]]
-                        then
-                                flag="yes"
-                        elif [[ $flag == "yes" ]]
-                        then
-                                answlen=(fullanswlen - 15)
-                                echo "${src:0:11} # [$lang] ${answ:4:answlen}"
-                                break
+#                flag="no"
+                enString=${src#*\#}
+                enString=${enString%[*}
+                echo -e "\\e[30;1m# [en]$enString[0m"
+                IFS=$'\n'
+                count=0
+                for answ in $(trans "$enString"); do
+                        if [ $count == 1 ]; then
+                                echo -e "\\e[32;1m# [ru] $answ\\e[0m"
                         fi
+                        count=$((count + 1))
                 done
         else
                 echo "$src"
